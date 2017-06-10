db67e2ee91168e3071abf8f30a6b3a4105cc2425
Kazuki Otao <kaootao@gmail.com>
2017-06-10 13:50:26 +0900

    twitterのタイムラインから焼肉を拾う部分の完成

--- yakiniku-bot/Program.cs
+++ yakiniku-bot/Program.cs
@@ -9,7 +9,7 @@ namespace yakinikubot
 
             Console.WriteLine("Load config file");
             new Config().Load();
-  
+
             Console.WriteLine("Get image url from web");
             var imageLoader = new ImageLoader();
             imageLoader.GetUrlsInFile();
@@ -19,9 +19,15 @@ namespace yakinikubot
             image.Load();
             var path = image.GetRandomFilePath();
 
+            var twitter = new TwitterController();
             var slack = new SlackController(true);
-            slack.Upload(path);
-	            
+
+            twitter.OnYakinikuReceived += (obj, e) => {
+                Console.WriteLine(e.ScreenName + "さんが焼肉と呟きました");
+                slack.Upload(e.ScreenName + "さんが焼肉と呟きました");
+                slack.Upload(path);
+            };
+            twitter.Connect();
         }
     }
 }
---
+++ yakiniku-bot/TwitterController.cs
@@ -0,0 +1,49 @@
+﻿﻿using System;
+using CoreTweet;
+using CoreTweet.Streaming;
+//using CoreTweet.Streaming.Reactive;
+
+namespace yakinikubot {
+    public class YakinikuEventArgs : EventArgs {
+        public string ScreenName;
+
+        public YakinikuEventArgs(string screenName) {
+            this.ScreenName = screenName;
+        }
+    }
+
+    public class TwitterController {
+    	
+
+        public delegate void YakinikuEventHandler(object sender, YakinikuEventArgs e);
+        public event YakinikuEventHandler OnYakinikuReceived;
+
+        public TwitterController() {
+
+        }
+
+        public void Connect() {
+            // Tokenの取得
+        	var tokens = CoreTweet.Tokens.Create(Config.consumerKey, Config.consumerSecret, Config.accessToken, Config.accessTokenSecret);
+
+            // Streamに接続
+        	var stream = tokens.Streaming.StartStream(CoreTweet.Streaming.StreamingType.User,
+                new StreamingParameters(replies => "all"));
+
+        	foreach(var message in stream) {
+    			if(message is StatusMessage) {
+        			var status = (message as StatusMessage).Status;
+        			// Console.WriteLine(string.Format("{0}:{1}", status.User.ScreenName, status.Text));
+        			if(status.Text.Contains("焼肉")) {
+                        if(OnYakinikuReceived != null) 
+                            OnYakinikuReceived(this, new YakinikuEventArgs(status.User.ScreenName));
+        			}
+    			} else if(message is EventMessage) {
+        			var ev = message as EventMessage;
+        			// Console.WriteLine(string.Format("{0}:{1}->{2}", ev.Event, ev.Source.ScreenName, ev.Target.ScreenName));
+    			}
+			}
+
+        }
+    }
+}
